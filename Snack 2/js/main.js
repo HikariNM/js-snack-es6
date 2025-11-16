a8ca64f26efd3e07acdcfc468f10ec0749bd9be6
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-16 14:17:30 +0100

    push oggetto  con nome a falli subiti

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -1,17 +1,19 @@
 const soccerTeams = [
-    {team: 'milan', puntiFatti: 0, falliSubiti: 0},
-    {team: 'inter', puntiFatti: 0, falliSubiti: 0},
-    {team: 'spezia', puntiFatti: 0, falliSubiti: 0},
-    {team: 'lazio', puntiFatti: 0, falliSubiti: 0},
+    {team: 'milan', scoredGoals: 0, receivedFouls: 0},
+    {team: 'inter', scoredGoals: 0, receivedFouls: 0},
+    {team: 'spezia', scoredGoals: 0, receivedFouls: 0},
+    {team: 'lazio', scoredGoals: 0, receivedFouls: 0},
 ]
 
+const teamsName =[];
+const teamsNamesFouls =[];
+
 for (let i = 0; i < soccerTeams.length; i++) {
-    soccerTeams[i].puntiFatti = randomNumber(0, 20);
-    soccerTeams[i].falliSubiti = randomNumber(1, 10);
+    soccerTeams[i].scoredGoals = randomNumber(0, 20);
+    soccerTeams[i].receivedFouls = randomNumber(1, 5);
 }
 console.log(soccerTeams)
 
-const teamsName =[];
 
 for (let i = 0; i < soccerTeams.length; i++) {
     const name = soccerTeams[i].team
@@ -19,6 +21,15 @@ for (let i = 0; i < soccerTeams.length; i++) {
 }
 console.log(teamsName)
 
+
+for (let i = 0; i < soccerTeams.length; i++) {
+    const name = soccerTeams[i].team
+    const receivedFouls = soccerTeams[i].receivedFouls
+    teamsNamesFouls.push({team: name, receivedFouls: receivedFouls})
+}
+console.log(teamsNamesFouls)
+
+
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1));
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
