e3e60effbaf2e07fa0e7cc00317f763539a8ae0d
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-16 12:39:18 +0100

    snack01

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -1,19 +1,19 @@
 const bikes = [
     {model: 'e-bike', weight: 22.5},
-    {model: 'da corsa', weight: 7.8},
     {model: 'MTB', weight: 12},
     {model: 'city', weight: 16},
+    {model: 'da corsa', weight: 7.8},
 ]
 
-let currentWeight = bikes[0].weight;
-let lowestBike = '';
+let lowestBike = bikes[0];
 
 for (let i = 0; i < bikes.length; i++) {
-    if ( bikes[i].weight < currentWeight ) {
-        currentWeight = bikes[i].weight;
+    if ( bikes[i].weight < lowestBike.weight ) {
         lowestBike = bikes[i];
         console.log(lowestBike)
     }
 
     
 }
+
+console.log(`La bici più leggera è quella da ${lowestBike.model} e pesa ${lowestBike.weight}kg`);
